ad65cb18ceb429266d08d4440bfb013ad9fa2dfb
nara04040 <nara04040@naver.com>
2024-06-24 23:03:14 +0900

    setting: 블로그 템플릿 작성

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { allPosts } from "@/.contentlayer/generated";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="prose dark:prose-invert">
+      {allPosts.map((post) => (
+        <article key={post._id}>
+          <Link href={post.slug}>
+            <h2>{post.title}</h2>
+          </Link>
+          {post.description && <p className="mb-1">{post.description}</p>}
+          {post.date && <p className="mt-0 mb-0">{new Date(post.date).toLocaleDateString()}</p>}
+        </article>
+      ))}
+    </div>
+  );
+}
